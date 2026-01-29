67387ee02e36d89f3fd50ebc6b07a488dd2736ae
IshankKumar31 <152838357+IshankKumar31@users.noreply.github.com>
2026-01-29 23:25:55 +0530

    Time: 3 ms (6.52%), Space: 8.6 MB (19.09%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,39 @@
+// class Solution {
+// public:
+//     bool rotateString(string s, string goal) {
+//         int n1=s.size();
+//         int n2=goal.size();
+//         if(n1 != n2){
+//             return false;
+//         }
+
+//         string temp= s+s;
+//         int pos=temp.find(goal);
+//         if(pos==-1){
+//             return false;
+//         }else{
+//             return true;
+//         }
+//     }
+// };
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int n1=s.size();
+        int n2=goal.size();
+        if(n1 != n2){
+            return false;
+        }
+
+        string temp= s+s;
+        for(int i=0;i<temp.size();i++){
+            if(goal== temp.substr(i,n1)){
+                return true;
+            }
+
+           
+        }
+        return false;
+       
+    }
+};
