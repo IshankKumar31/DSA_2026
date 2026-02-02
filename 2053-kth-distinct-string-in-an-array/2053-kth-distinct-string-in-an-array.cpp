b22efd92b3b4067cf2944dcb3ece2cda89c8a202
IshankKumar31 <152838357+IshankKumar31@users.noreply.github.com>
2026-02-02 16:40:44 +0530

    Time: 0 ms (100%), Space: 18.3 MB (49.88%) - LeetHub

--- 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.cpp
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.cpp
@@ -9,16 +9,17 @@ public:
             freq[arr[i]]++;
         }
         int count=0;
+        string ans="";
         for(int i=0;i<n;i++){
             if(freq[arr[i]]==1){
                 count++;
                 if(count==k){
-                    return arr[i];
+                    ans=arr[i];
                 }
 
             }
         }
-        return "";
+        return ans;
 
 
     }
