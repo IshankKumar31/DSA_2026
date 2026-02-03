44c5215e516f4a67aa95011eda55b8ca3ca9174a
IshankKumar31 <152838357+IshankKumar31@users.noreply.github.com>
2026-02-03 11:48:16 +0530

    Time: 0 ms (100%), Space: 69.7 MB (25.97%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,25 +1,52 @@
+// class Solution {
+// public:
+//     int longestOnes(vector<int>& nums, int k) {
+//         int n= nums.size();
+//         int left=0;
+//         int maxlen=0;
+//         int count=0;
+
+//         for(int right=0;right<n;right++){
+//             if(nums[right]==0){
+//                 count++;
+//             }
+//             while(count>k){
+//                 if(nums[left] ==0){
+//                     count--;
+//                 }
+//                 left++;
+//             }
+//             int len=right-left+1;
+//             maxlen=max(maxlen,len);
+//         }
+
+//         return maxlen;
+//     }
+// };
+
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        int n= nums.size();
-        int left=0;
-        int maxlen=0;
-        int count=0;
-
-        for(int right=0;right<n;right++){
-            if(nums[right]==0){
-                count++;
+        int left = 0;
+        int right = 0;
+        
+        for (right = 0; right < nums.size(); right++) {
+       
+            if (nums[right] == 0) {
+                k--;
             }
-            while(count>k){
-                if(nums[left] ==0){
-                    count--;
+            
+          
+            if (k < 0) {
+                if (nums[left] == 0) {
+                    k++;
                 }
                 left++;
             }
-            int len=right-left+1;
-            maxlen=max(maxlen,len);
         }
-
-        return maxlen;
+        return right - left;
     }
 };
+              
+        
+       
