e54b493174e783d50ebe34fcbcb8e36bccb9f80b
IshankKumar31 <152838357+IshankKumar31@users.noreply.github.com>
2026-02-07 13:04:51 +0530

    Time: 1 ms (73.26%), Space: 23.4 MB (64.33%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.cpp
@@ -0,0 +1,32 @@
+class MinStack {
+    stack<pair<int,int>> st;
+
+public:
+    MinStack() {
+
+    }
+    
+    void push(int val) {
+        if(st.empty()) {
+            st.push({val, val});
+        }
+        else {
+            int currMin = min(val, st.top().second);
+            st.push({val, currMin});
+        }
+    }
+    
+    void pop() {
+        if(!st.empty()) {
+            st.pop();
+        }
+    }
+    
+    int top() {
+        return st.top().first;
+    }
+    
+    int getMin() {
+        return st.top().second;
+    }
+};
